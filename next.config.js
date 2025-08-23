b663c41c01695baabafc38840b2389d66151b669
RyanChen <127590951+RC918@users.noreply.github.com>
2025-08-24 02:24:16 +0800

    Clean codebase: Create Next.js config - 2025-08-23 14:24:15

--- next.config.js
+++ next.config.js
@@ -1,9 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
+  output: 'export',
   trailingSlash: true,
   images: {
     unoptimized: true
+  },
+  env: {
+    NEXT_PUBLIC_STAGE: process.env.NEXT_PUBLIC_STAGE || 'prod',
+    NEXT_PUBLIC_API_BASE: process.env.NEXT_PUBLIC_API_BASE || 'https://api.morningai.me'
   }
 }
 
